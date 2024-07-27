9faa17763ca8f74cc72206fbd5b222babda3cd3e
Ha4lan <kaaiontop@gmail.com>
2024-07-28 00:14:25 +0900

    aboutme section

--- components/About.js
+++ components/About.js
@@ -2,11 +2,12 @@
 import React from 'react';
 import Image from 'next/image'
 import { motion } from 'framer-motion';
+import SkillData from '@/constants/skillData';
 
 const About = () => {
     return (
-        <section className='bg-gray-800 text-white'>
-            <h2 className='text-center text-4xl font-bold pt-[2rem] pb-[2rem]'>About <motion.span whileInView={{fontSize: "50px", content: "value"}}
+        <section className='bg-gray-800 text-white pb-[3rem]'>
+            <h2 className='text-center text-4xl font-bold pt-[3rem] pb-[3rem]'>About <motion.span whileInView={{fontSize: "50px", content: "value"}}
             className='bg-gradient-to-r from-indigo-500 to-indigo-400 bg-clip-text text-fill-transparent'>ME</motion.span></h2>
             <div className='profile flex justify-center gap-[50px]'>
                 {/* left */}
@@ -18,6 +19,17 @@ const About = () => {
                     <h3 className='text-3xl mb-[0.75rem] font-bold'>Hasilan</h3>
                     <p>Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。Twittererです。</p>
                 </div>
+            </div>
+            <h2 className='text-center text-4xl font-bold pt-[4rem] pb-[3rem]'>My <motion.span whileInView={{fontSize: "50px", content: "value"}}
+            className='bg-gradient-to-r from-indigo-500 to-indigo-400 bg-clip-text text-fill-transparent'>SKILL</motion.span></h2>
+            <div className='flex justify-center gap-[2rem]'>
+            {SkillData.map((skill, index) => (
+                <div key={index} className="p-[2.5rem] bg-gradient-to-tr from-indigo-500 to-indigo-400">
+                    <div className="icon">{skill.icon}</div>
+                    <div className="name">{skill.name}</div>
+                    <div className="level">{skill.skill}</div>
+                </div>
+            ))}
             </div>
         </section>
     );
---
+++ constants/skillData.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FaCss3, FaHtml5, FaJs } from "react-icons/fa";
+
+const SkillData = [
+    {
+        icon: <FaHtml5 />,
+        name: "HTML5",
+        skill: "1"
+    },
+    {
+        icon: <FaCss3 />,
+        name: "CSS3",
+        skill: "0.6"
+    },
+    {
+        icon: <FaJs />,
+        name: "JavaScipt",
+        skill: "0.7"
+    }
+]
+
+export default SkillData;

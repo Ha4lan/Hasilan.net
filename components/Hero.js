2b9570602e849ee0e09329977efe175f4480a38c
Ha4lan <kaaiontop@gmail.com>
2024-07-27 12:26:42 +0900

    Hero part is completed except Header

--- components/Hero.js
+++ components/Hero.js
@@ -1,16 +1,23 @@
 'use client'
-import React from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 export default function Hero() {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
         <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center">
-            <motion.div whileHover={{scale: 1.2}} whileTap={{scale:1.1}} className="text-gray-100 text-left mr-[30rem]">
-                <span className='text-6xl font-bold block bg-gradient-to-r from-indigo-100 bg-clip-text text-fill-transparent'>Welcome to</span>
-                <span className='text-8xl font-extrabold block bg-gradient-to-r from-indigo-500 to-indigo-400 bg-clip-text text-fill-transparent'>Hasilan.net!</span>
-                <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 1.1 }} drag="x" dragConstraints={{ left: -100, right: 100 }} />
-            </motion.div>
+        <motion.div onHoverStart={() => setIsHovered(true)} onHoverEnd={() => setIsHovered(false)} whileHover={{ scale: 1.2 }} whileTap={{ scale: 1.1 }}
+        className="text-gray-100 text-left mr-[30rem] cursor-default select-none">
+            <span className='text-6xl font-bold block bg-gradient-to-r from-indigo-100 bg-clip-text text-fill-transparent'>
+                Welcome to
+            </span>
+            <motion.span animate={{ scale: isHovered ? 1.1 : 1 }}
+            className='text-8xl font-extrabold block bg-gradient-to-r from-indigo-500 to-indigo-400 bg-clip-text text-fill-transparent'>
+                Hasilan.net!
+            </motion.span>
+        </motion.div>
         </header>
       )
 }

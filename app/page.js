e5ead95c150ea2c43acf14274b5f55c3ad41eadd
Ha4lan <kaaiontop@gmail.com>
2024-07-27 14:55:58 +0900

    save

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Image from "next/image";
-import { ChakraProvider } from "@chakra-ui/react";
 
-export default function Home({Component, pageProps}) {
+export default function Home() {
   return (
-    <ChakraProvider>
+    <>
       <Hero />
-    </ChakraProvider>
+      <About />
+    </>
   );
 }
---
+++ components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section className='bg-gray-800 text-white'>
+            <h2 className='text-center text-4xl font-bold pt-[2rem] pb-[2rem]'>About <span className='bg-gradient-to-r from-indigo-500 to-indigo-400 bg-clip-text text-fill-transparent'>me</span></h2>
+        </section>
+    );
+}
+
+export default About;
